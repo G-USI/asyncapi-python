17d7bfb7d589b4afe9f33f3db5d665c99a183b11
Yaroslav Petrov <yaroslav.v.petrov@gmail.com>
2024-10-20 17:48:31 +0000

    Add more code to Consumer

--- src/asyncapi_python/amqp/consumer.py
+++ src/asyncapi_python/amqp/consumer.py
@@ -1,9 +1,13 @@
-from .message_handler import AbstractMessageHandler
+import asyncio
+from .message_handler import AbstractMessageHandler, MessageHandler, RpcMessageHandler
 from .connection import connection_pool
 from asyncio import Future
-from typing import Awaitable, Callable
+from typing import Awaitable, Callable, TypeVar
 from pydantic import BaseModel
 from logging import getLogger
+
+T = TypeVar("T", bound=BaseModel)
+U = TypeVar("U", bound=BaseModel)
 
 
 class Consumer:
@@ -12,18 +16,32 @@ class Consumer:
         self.__handlers: dict[str, AbstractMessageHandler] = {}
         self.__logger = getLogger(__name__)
 
-    async def run(self):
+    async def run(self, timeout: float | None = None):
         conn = connection_pool(self.__amqp_uri)
-        return await Future()
+        if timeout is not None:
+            await asyncio.sleep(timeout)
+        else:
+            await Future()
 
     def on(
         self,
         *,
         key: str,
-        input_type: type[BaseModel],
-        output_type: type[BaseModel] | None,
-        callback: Callable[[BaseModel], Awaitable[BaseModel | None]],
+        input_type: type[T],
+        output_type: type[U] | None,
+        callback: Callable,
     ):
+        handler: AbstractMessageHandler
         if key in self.__handlers:
             raise AssertionError(f"Only one handler for `{key}` is allowed")
-        self.__handlers[key] = AbstractMessageHandler()
+        if output_type is None:
+            handler = MessageHandler(key, callback=callback, input_type=input_type)
+        else:
+            handler = RpcMessageHandler(
+                key,
+                callback=callback,
+                input_type=input_type,
+                output_type=output_type,
+                channel=None,
+            )
+        self.__handlers[key] = handler
