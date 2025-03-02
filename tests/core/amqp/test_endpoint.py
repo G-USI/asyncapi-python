30b69cde19dd0bcea6ed0728701fc21ccc7807a7
Yaroslav Petrov <yaroslav.v.petrov@gmail.com>
2025-03-02 12:06:21 +0000

    Add rejection tests for endpoints

--- tests/core/amqp/test_endpoint.py
+++ tests/core/amqp/test_endpoint.py
@@ -16,6 +16,7 @@
 from asyncio import Future
 import asyncio
 from collections import defaultdict
+import json
 
 from pydantic import BaseModel, RootModel
 from asyncapi_python.amqp import (
@@ -27,6 +28,7 @@ from asyncapi_python.amqp import (
     Receiver,
     RpcReceiver,
 )
+from asyncapi_python.amqp import Rejection, RejectedError
 from asyncapi_python.amqp.utils import encode_message, decode_message
 from aio_pika.abc import AbstractIncomingMessage
 from uuid import uuid4
@@ -55,6 +57,7 @@ def rpc_operation() -> Operation:
         reply_type=AddResponse,
         exchange_name=None,
         exchange_type="default",
+        debug_auto_delete=True,
     )
 
 
@@ -67,6 +70,33 @@ def operation() -> Operation:
         reply_type=None.__class__,
         exchange_name=None,
         exchange_type="default",
+        debug_auto_delete=True,
+    )
+
+
+@pytest.fixture(scope="function")
+def err_operation() -> Operation:
+    return Operation(
+        name="operations/error",
+        routing_key="testErrorQueue",
+        message_type=Log,
+        reply_type=None.__class__,
+        exchange_name=None,
+        exchange_type="default",
+        debug_auto_delete=True,
+    )
+
+
+@pytest.fixture(scope="function")
+def err_rpc_operation() -> Operation:
+    return Operation(
+        name="operations/add/error",
+        routing_key="testErrorRpcQueue",
+        message_type=AddRequest,
+        reply_type=AddResponse,
+        exchange_name=None,
+        exchange_type="default",
+        debug_auto_delete=True,
     )
 
 
@@ -75,8 +105,8 @@ def correlation_ids() -> dict[str, Future[AbstractIncomingMessage]]:
     return defaultdict(lambda: Future())
 
 
-@pytest.fixture(scope="function")
 def params(
+    app_id: str,
     amqp_pool: AmqpPool,
     correlation_ids: dict[str, Future[AbstractIncomingMessage]],
 ) -> EndpointParams:
@@ -85,14 +115,29 @@ def params(
         register_correlation_id=lambda: ((uuid := str(uuid4()), correlation_ids[uuid])),
         encode=encode_message,
         decode=decode_message,
-        app_id="app-1",
+        app_id=app_id,
         stop_application=lambda: exit(-1),
     )
 
 
-async def test_queue(params: EndpointParams, operation: Operation):
-    producer: Sender[Log] = Sender(operation, params)
-    consumer: Receiver[Log] = Receiver(operation, params)
+@pytest.fixture(scope="function")
+def params_0(amqp_pool, correlation_ids):
+    return params("app-0", amqp_pool, correlation_ids)
+
+
+@pytest.fixture(scope="function")
+def params_1(amqp_pool, correlation_ids):
+    return params("app-1", amqp_pool, correlation_ids)
+
+
+@pytest.fixture(scope="function")
+def params_2(amqp_pool, correlation_ids):
+    return params("app-2", amqp_pool, correlation_ids)
+
+
+async def test_queue(params_1: EndpointParams, operation: Operation):
+    producer: Sender[Log] = Sender(operation, params_1)
+    consumer: Receiver[Log] = Receiver(operation, params_1)
 
     count = [0]
 
@@ -114,13 +159,15 @@ async def test_queue(params: EndpointParams, operation: Operation):
 
 
 async def test_rpc(
-    params: EndpointParams,
+    params_0: EndpointParams,
     rpc_operation: Operation,
     amqp_pool: AmqpPool,
     correlation_ids: dict[str, Future[AbstractIncomingMessage]],
 ):
-    producer: RpcSender[AddRequest, AddResponse] = RpcSender(rpc_operation, params)
-    consumer: RpcReceiver[AddRequest, AddResponse] = RpcReceiver(rpc_operation, params)
+    producer: RpcSender[AddRequest, AddResponse] = RpcSender(rpc_operation, params_0)
+    consumer: RpcReceiver[AddRequest, AddResponse] = RpcReceiver(
+        rpc_operation, params_0
+    )
 
     async def on_request(msg: AddRequest) -> AddResponse:
         return AddResponse(root=msg.a + msg.b)
@@ -128,6 +175,7 @@ async def test_rpc(
     async def on_reply(msg: AbstractIncomingMessage):
         future = correlation_ids.pop(msg.correlation_id or "")
         future.set_result(msg)
+        await msg.ack()
 
     consumer(on_request)
 
@@ -135,10 +183,95 @@ async def test_rpc(
     await consumer.start()
 
     async with amqp_pool.acquire() as ch:
-        q = await ch.declare_queue(params.reply_queue_name, auto_delete=True)
+        q = await ch.declare_queue(params_0.reply_queue_name, exclusive=True)
         await q.consume(on_reply)
 
     assert await producer(AddRequest(a=1, b=2)) == AddResponse(root=3)
     assert await producer(AddRequest(a=3, b=2)) == AddResponse(root=5)
+    assert await producer(AddRequest(a=4, b=6)) == AddResponse(root=10)
+    assert await producer(AddRequest(a=3, b=1)) == AddResponse(root=4)
+
+
+async def test_reject(
+    params_1: EndpointParams, err_operation: Operation, amqp_pool: AmqpPool
+):
+    producer: Sender[Log] = Sender(err_operation, params_1)
+    consumer: Receiver[Log] = Receiver(err_operation, params_1)
+
+    error_sent = [0]
+
+    async def on_log(_: Log):
+        raise Rejection("Access to logging denied")
+
+    consumer(on_log)
+
+    await producer.start()
+    await consumer.start()
+
+    async def on_error(msg: AbstractIncomingMessage):
+        assert not msg.correlation_id
+        payload = json.loads(msg.body)
+        err, orig = payload.get("error", None), payload.get("original_message", None)
+        assert err
+        assert orig
+        error_sent[0] += 1
+        await msg.ack()
+
+    async with amqp_pool.acquire() as ch:
+        q = await ch.declare_queue(params_1.error_queue_name, exclusive=True)
+        await q.consume(on_error)
+
+    log = Log(content="Something went wrong")
+    await producer(log)
+
+    await asyncio.sleep(0.2)
+    assert error_sent[0] == 1
+
+
+async def test_err_rpc(
+    params_2: EndpointParams,
+    err_rpc_operation: Operation,
+    amqp_pool: AmqpPool,
+    correlation_ids: dict[str, Future[AbstractIncomingMessage]],
+):
+
+    producer: RpcSender[AddRequest, AddResponse] = RpcSender(
+        err_rpc_operation, params_2
+    )
+    consumer: RpcReceiver[AddRequest, AddResponse] = RpcReceiver(
+        err_rpc_operation, params_2
+    )
+
+    async def on_request(msg: AddRequest) -> AddResponse:
+        if msg.b == 2:
+            raise Rejection("This service rejects when b=2")
+        return AddResponse(root=msg.a + msg.b)
+
+    async def on_reply(msg: AbstractIncomingMessage):
+        future = correlation_ids.pop(msg.correlation_id or "")
+        future.set_result(msg)
+
+    async def on_error(msg: AbstractIncomingMessage):
+        payload = json.loads(msg.body)
+        future = correlation_ids.pop(msg.correlation_id or "")
+        future.set_exception(
+            RejectedError(payload["error"], payload["original_message"])
+        )
+
+    consumer(on_request)
+
+    await producer.start()
+    await consumer.start()
+
+    async with amqp_pool.acquire() as ch:
+        q = await ch.declare_queue(params_2.reply_queue_name, exclusive=True)
+        await q.consume(on_reply)
+        q = await ch.declare_queue(params_2.error_queue_name, exclusive=True)
+        await q.consume(on_error)
+
+    with pytest.raises(RejectedError):
+        await producer(AddRequest(a=1, b=2))
+    with pytest.raises(RejectedError):
+        await producer(AddRequest(a=3, b=2))
     assert await producer(AddRequest(a=4, b=6)) == AddResponse(root=10)
     assert await producer(AddRequest(a=3, b=1)) == AddResponse(root=4)
