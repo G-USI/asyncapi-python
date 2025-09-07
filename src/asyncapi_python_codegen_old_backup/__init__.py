67cfee90217ce27e801f69b849d8f2afe17f1177
Yaroslav Petrov <yaroslav.v.petrov@gmail.com>
2025-09-07 14:16:10 +0000

    Drop old codegen

--- src/asyncapi_python_codegen_old_backup/__init__.py
+++
@@ -1,8 +0,0 @@
-"""AsyncAPI Python Code Generator."""
-
-from .generator import CodeGenerator
-from .parser import extract_all_operations, load_document_info
-from .cli import app
-
-__version__ = "0.1.0"
-__all__ = ["CodeGenerator", "extract_all_operations", "load_document_info", "app"]
--- src/asyncapi_python_codegen_old_backup/parser/__init__.py
+++
@@ -1,6 +0,0 @@
-"""AsyncAPI dataclass-based parser using kernel.document types."""
-
-from .types import YamlDocument
-from .document_loader import extract_all_operations, load_document_info
-
-__all__ = ["YamlDocument", "extract_all_operations", "load_document_info"]
